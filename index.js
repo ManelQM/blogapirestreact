87f526d0cfe99a6c84d31e25369cc76aac73bc95
Manel <quemonmanel@gmail.com>
2024-06-27 18:58:16 +0200

    feat:first node server steps

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Ieeeeeeeeeeeeeee"); 
+const {connection} = require("./db/connection"); 
+const express = require("express"); 
+
+
+
+// Inicializar app
+console.log("Node app running"); 
+
+// Conectar con la base de datos
+connection(); 
+
+// Crear servidor Node 
+
+const app = express(); 
+
+// Configurar CORS 
+
+app.use(cors()); 
