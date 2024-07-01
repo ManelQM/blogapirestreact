cca3e63308adb4722c556a8bd4bd713281bdded0
Manel <quemonmanel@gmail.com>
2024-07-01 11:55:41 +0200

    chore:checking demo endpoint response

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const {connection} = require("./db/connection"); 
 const express = require("express"); 
+const cors = require("cors"); 
 
 
 
 // Inicializar app
 console.log("Node app running"); 
 
-// Conectar con la base de datos
-connection(); 
-
 // Crear servidor Node 
 
 const app = express(); 
+const PORT = 3991
 
 // Configurar CORS 
 
 app.use(cors()); 
+
+// Convertir body a objeto js
+
+app.use(express.json()); 
+
+// Rutas
+
+app.get("/probando", (req, res) => {
+    console.log("Se ha ejecutado el endpoint de prueba"); 
+    
+    return res.status(200).json({
+        status: "success",
+        message: "Esto es la respuesta del endpoint en formato json"
+    })
+})
+
+// Crear servidor y escuchar peticiones http
+
+app.listen(PORT , () => {
+    console.log("Running at port => "+ PORT)
+})
+
+// Conectar con la base de datos
+connection();
